d94ecf24ba55df087bdf34f4529a33c690cd091d
Albert <albert.gee.ca@gmail.com>
2022-10-26 11:17:42 -0700

    Change bot_slider_widget.dart

--- lib/modules/bot/widgets/bot_slider_widget.dart
+++ lib/modules/bot/widgets/bot_slider_widget.dart
@@ -6,28 +6,42 @@ class BotSliderWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return Column(
-      children: [
-        const SizedBox(height: 15.0),
+    return Container(
 
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: 30,
-              height: 5,
-              decoration: BoxDecoration(
-                color: Colors.grey[300],
-                borderRadius: const BorderRadius.all(Radius.circular(12.0))
-              ),
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.15),
+            spreadRadius: 2.5,
+            blurRadius: 10,
+            offset: const Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Column(
+          children: [
+            const SizedBox(height: 10.0),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 30,
+                  height: 5,
+                  decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: const BorderRadius.all(Radius.circular(12.0))
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
 
-        const SizedBox(
-          height: 15.0,
-        )
-      ]
+            const SizedBox(
+              height: 10.0,
+            )
+          ]
+      ),
     );
 
   }
