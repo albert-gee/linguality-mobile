c858ee0377a9c8c4427baf7c5f5df2785fa580dd
Albert <albert.gee.ca@gmail.com>
2022-10-30 01:54:41 -0700

    Remove User from Home page

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -2,15 +2,13 @@ import 'package:bot/widgets/bot_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
-import '../../models/user.dart';
 import 'board.dart';
 import '../../providers/bot_provider.dart';
 
 class HomePage extends StatelessWidget {
-    const HomePage({super.key, required this.title, required this.user});
+    const HomePage({super.key, required this.title});
 
   final String title;
-  final User user;
 
   final double _panelHeightOpen = 50;
 
@@ -28,7 +26,7 @@ class HomePage extends StatelessWidget {
         defaultPanelState: PanelState.OPEN,
         controller: PanelController(),
 
-        body: Board(user: user),
+        body: const Board(),
         panel: BotWidget(botProvider: BotProvider()),
 
       ),
