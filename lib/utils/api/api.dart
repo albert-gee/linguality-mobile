86c7983e5924d793d932c776438cb46bb32df56c
Albert <albert.gee.ca@gmail.com>
2022-11-12 00:36:30 -0800

    Add postDownload method to API util

--- lib/utils/api/api.dart
+++ lib/utils/api/api.dart
@@ -5,8 +5,8 @@ class Api {
   final Dio _dio = Dio();
 
   Future<ApiResponse> post(
-      {required url, Map<String, dynamic>? data, String? jwt}) async {
-    Response response = await _dio.post("$url",
+      {required String url, Map<String, dynamic>? data, String? jwt}) async {
+    Response response = await _dio.post(url,
         data: data,
         options: Options(
             validateStatus: (status) {
@@ -21,5 +21,38 @@ class Api {
         statusCode: response.statusCode,
         statusMessage: response.statusMessage,
         data: response.data);
+  }
+
+  /// Send post request, download file, and save to temporary directory
+  Future<ApiResponse> postDownload(
+      {required String url, required String tempFilePath, Map<String, dynamic>? data, String? jwt}) async {
+
+    ApiResponse apiResponse;
+
+    try {
+      Response response = await _dio.download(url,
+          tempFilePath,
+          data: data,
+          options: Options(
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": (jwt != null ? "Bearer $jwt" : null),
+            }),
+      );
+
+      apiResponse = ApiResponse(
+          statusCode: response.statusCode,
+          statusMessage: response.statusMessage,
+          data: tempFilePath);
+
+    } catch (e) {
+      print(e);
+      apiResponse = ApiResponse(
+          statusCode: 500,
+          statusMessage: "Internal Server Error",
+          data: null);
+    }
+    return apiResponse;
   }
 }
