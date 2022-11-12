923bb322e4d4ff2a5971f46f8e0210ae3ea49268
Albert <albert.gee.ca@gmail.com>
2022-11-12 00:34:39 -0800

    Add TextToSpeechProvider

---
+++ lib/providers/text_to_speech_provider.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:bot/providers/text_to_speech_provider_contract.dart';
+import 'package:path_provider/path_provider.dart';
+
+import '../configuration/configuration.dart';
+import '../utils/api/api.dart';
+import '../utils/api/api_response.dart';
+import '../utils/key_storage.dart';
+
+class TextToSpeechProvider extends TextToSpeechProviderContract {
+  final Configuration settings = Configuration();
+  final KeyStorage keyStorage = KeyStorage();
+  final Api api = Api();
+
+  @override
+  Future<String> convert(String inputText) async {
+    var token = await keyStorage.read(settings.secureStorageKeyAccessToken);
+    if (token != null) {
+
+      String audioFileName = "${inputText.hashCode}.mp3";
+
+      Directory tempDir = await getTemporaryDirectory();
+      String tempFilePath = "${tempDir.path}/$audioFileName";
+
+      if (File(tempFilePath).existsSync()) {
+        return tempFilePath;
+      } else {
+        ApiResponse apiResponse = await api.postDownload(
+            url: '${settings.apiServerUrl}/tts',
+            tempFilePath: tempFilePath,
+            data: {'inputText': inputText},
+            jwt: token);
+
+        if (apiResponse.statusCode == 200 && apiResponse.data == tempFilePath) {
+          return apiResponse.data;
+        } else {
+          throw Exception('Unsuccessful text to speech conversion');
+        }
+      }
+    } else {
+      throw Exception("Unauthenticated");
+    }
+  }
+}
