9ff2042e23d5639f5fb967bcaa2f826573991dcc
Albert <albert.gee.ca@gmail.com>
2022-11-13 23:55:24 -0800

    Change Article model - add id abd text

--- board/lib/models/article.dart
+++ board/lib/models/article.dart
@@ -1,6 +1,8 @@
 class Article {
+  final String id;
   final String title;
   final String imageUrl;
+  final String text;
 
-  Article({required this.title, required this.imageUrl});
+  Article({required this.id, required this.title, required this.imageUrl, required this.text});
 }
