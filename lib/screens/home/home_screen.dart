4cdbb349573568e019d1f448c5d394b5602ad66d
Albert <albert.gee.ca@gmail.com>
2022-11-20 22:43:38 -0800

    Update Home screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,23 +1,27 @@
-import 'package:board/widgets/board.dart';
+import 'package:board/providers/article_paragraph_to_speech_provider_contract.dart';
+import 'package:board/providers/article_provider_contract.dart';
+import 'package:board/widgets/board_widget.dart';
 import 'package:bot/providers/bot_provider_contract.dart';
 import 'package:bot/providers/text_to_speech_provider_contract.dart';
 import 'package:bot/widgets/bot_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
-import '../../providers/article_provider.dart';
-
 class HomeScreen extends StatelessWidget {
   const HomeScreen({
     super.key,
     required this.title,
     required this.botProvider,
+    required this.articleProvider,
     required this.textToSpeechProvider,
+    required this.articleParagraphToSpeechProvider,
   });
 
   final String title;
   final BotProviderContract botProvider;
+  final ArticleProviderContract articleProvider;
   final TextToSpeechProviderContract textToSpeechProvider;
+  final ArticleParagraphToSpeechProviderContract articleParagraphToSpeechProvider;
 
   final double _panelHeightOpen = 50;
 
@@ -42,8 +46,9 @@ class HomeScreen extends StatelessWidget {
         maxHeight: panelHeightClosed,
         defaultPanelState: PanelState.OPEN,
         controller: PanelController(),
-        body: Board(
-          articleProvider: ArticleProvider(),
+        body: BoardWidget(
+          articleProvider: articleProvider,
+          articleParagraphToSpeechProvider: articleParagraphToSpeechProvider,
         ),
         panel: BotWidget(
           botProvider: botProvider,
